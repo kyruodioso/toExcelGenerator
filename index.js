f62d72a948b601ae84ed31a5460b9f5eddf24cbc
kyruodioso <kanu_nirvana@hotmail.com>
2023-09-20 13:06:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+document.onload = () => {
+    converterSheet()
+}
+
+function converterSheet() {
+    const fileInput = document.getElementById('fileInput');
+    let containerSheet = document.getElementById('containerSheet');
+    const file = fileInput.files[0];
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: 'binary' });
+
+        const jsonData = [];
+        workbook.SheetNames.map(function (sheetName) {
+            const worksheet = workbook.Sheets[sheetName];
+            const sheetData = XLSX.utils.sheet_to_json(worksheet, { raw: false });
+            jsonData.push({ [sheetName]: sheetData });
+        });
+
+        const jsonOutput = JSON.stringify(jsonData, null, 2);
+        console.log(jsonOutput);
+
+
+        let resJson = JSON.parse(jsonOutput).map(function (sheet) {
+
+            let sheetTable = '';
+
+            for (let index = 0; index < sheet.PortfolioSummary.length; index++) {
+                const element = sheet.PortfolioSummary[index];
+                console.log(element)
+
+                sheetTable += `<h2>${element.__EMPTY_1}</h2>`
+            }
+            containerSheet.innerHTML = sheetTable;
+
+        })
+
+
+    };
+
+    reader.readAsBinaryString(file);
+}
